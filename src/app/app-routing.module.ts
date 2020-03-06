32b18e868ae28783c9d638bc699f9c8d13b3961f
Sergio <sermartguer@gmail.com>
2020-03-06 14:20:55 +0100

    Layout component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { LayoutComponent } from "./layout/layout.component";
 
 const routes: Routes = [
   {
     path: "",
-    redirectTo: "/home",
-    pathMatch: "full"
-  },
-  {
-    path: "home",
-    loadChildren: () => import("./home/home.module").then(m => m.HomeModule)
+    component: LayoutComponent,
+    children: [
+      {
+        path: "",
+        redirectTo: "/home",
+        pathMatch: "full"
+      },
+      {
+        path: "home",
+        loadChildren: () => import("./home/home.module").then(m => m.HomeModule)
+      }
+    ]
   }
 ];
 
